6e4dbb1ef0518deac9be03a249d99dd97d094d58
Bruno Krebs <bruno.krebs@auth0.com>
2019-03-28 16:30:33 -0300

    migrating to androidx;

--- app/src/main/java/com/auth0/todo/MainActivity.java
+++ app/src/main/java/com/auth0/todo/MainActivity.java
@@ -1,6 +1,6 @@
 package com.auth0.todo;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
