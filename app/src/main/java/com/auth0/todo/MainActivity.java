028549a9fe00bdb8bef4fe6e353dc22a4bb4c7e5
Bruno Krebs <bruno.krebs@auth0.com>
2019-03-28 18:15:04 -0300

    static to-do list;

--- app/src/main/java/com/auth0/todo/MainActivity.java
+++ app/src/main/java/com/auth0/todo/MainActivity.java
@@ -2,12 +2,20 @@ package com.auth0.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import com.auth0.todo.util.ToDoListAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private ToDoListAdapter toDoListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // create and configure the adapter
+        this.toDoListAdapter = new ToDoListAdapter(this);
+        ListView microPostsListView = findViewById(R.id.micro_posts);
+        microPostsListView.setAdapter(toDoListAdapter);
     }
 }
---
+++ app/src/main/java/com/auth0/todo/util/ToDoListAdapter.java
@@ -0,0 +1,52 @@
+package com.auth0.todo.util;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.auth0.todo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoListAdapter extends BaseAdapter {
+    private LayoutInflater inflater;
+    private List<String> toDoList = new ArrayList<>();
+
+    public ToDoListAdapter(Context context) {
+        toDoList.add("My first task");
+        toDoList.add("My second task");
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        String message = (String) getItem(position);
+        if (view == null) {
+            view = inflater.inflate(R.layout.to_do_item, null);
+        }
+
+        TextView textView = view.findViewById(R.id.to_do_message);
+        textView.setText(message);
+
+        return view;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return toDoList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getCount() {
+        return toDoList.size();
+    }
+}
